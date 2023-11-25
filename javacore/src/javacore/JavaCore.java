9c6a3b054a0c726f90f275c27936b3fcacf0641c
umadevi-vasantha <umadevi4174@gmail.com>
2023-11-25 14:09:36 +0530

    1st Commit

---
+++ javacore/src/javacore/JavaCore.java
@@ -0,0 +1,11 @@
+package javacore;
+
+public class JavaCore {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Java CORE revision");
+		System.out.println("1st Commit");
+	}
+
+}
